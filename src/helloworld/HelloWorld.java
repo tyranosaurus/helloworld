b0006f1a16580f3a6e526de8407c80cec3447b3e
ChoiYoungjin <tyranosaurus@nate.com>
2016-03-02 16:58:49 +0900

    커밋이다

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -1,8 +1,8 @@
-package helloworld;
+﻿package helloworld;
 
 public class HelloWorld {
 	public static void main(String[] args){
-		System.out.println("Hello World2");
+		System.out.println("Hellojjj World2");
 		System.out.println("영진오빠 섹시해요");
 	}
 }
